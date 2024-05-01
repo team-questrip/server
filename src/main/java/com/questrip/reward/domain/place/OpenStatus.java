9d00520b7376169aa474cbb731fede8d3a6ba26f
dorian <doriantheblue@naver.com>
2024-05-01 23:58:01 +0900

    feat. Place domain

---
+++ src/main/java/com/questrip/reward/domain/place/OpenStatus.java
@@ -0,0 +1,7 @@
+package com.questrip.reward.domain.place;
+
+public enum OpenStatus {
+    OPEN,
+    CLOSE,
+    UNKNOWN
+}
---
+++ src/main/java/com/questrip/reward/domain/place/Place.java
@@ -0,0 +1,30 @@
+package com.questrip.reward.domain.place;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Place {
+    private Long id;
+    private String googlePlaceId;
+    private String placeName;
+    private String primaryType;
+    private String formattedAddress;
+    private LatLng location;
+    private List<String> openingHours;
+    private OpenPeriods openPeriods;
+
+    @Builder
+    private Place(Long id, String googlePlaceId, String placeName, String primaryType, String formattedAddress, LatLng location, List<String> openingHours, List<Period> openPeriods) {
+        this.id = id;
+        this.googlePlaceId = googlePlaceId;
+        this.placeName = placeName;
+        this.primaryType = primaryType;
+        this.formattedAddress = formattedAddress;
+        this.location = location;
+        this.openingHours = openingHours;
+        this.openPeriods = new OpenPeriods(openPeriods);
+    }
+}
