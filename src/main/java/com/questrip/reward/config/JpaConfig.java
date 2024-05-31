7ccb2ac9e09c679c275e54a9225245091a9fa8ed
dorian <doriantheblue@naver.com>
2024-05-31 22:42:36 +0900

    refactor. 의존성 분리

--- src/main/java/com/questrip/reward/config/JpaConfig.java
+++ src/main/java/com/questrip/reward/config/JpaConfig.java
@@ -2,8 +2,10 @@ package com.questrip.reward.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.questrip.reward.storage.mysql")
 public class JpaConfig {
 }
