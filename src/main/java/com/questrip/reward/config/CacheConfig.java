6fae8ccb92b19cd37fbc16d3eaaa4d5fe8522d9a
harok <doriantheblue@naver.com>
2024-09-17 22:00:05 +0900

    feat: Category 응답 Cache 추가

---
+++ src/main/java/com/questrip/reward/config/CacheConfig.java
@@ -0,0 +1,30 @@
+package com.questrip.reward.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("placeCounts");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(10)
+                .maximumSize(50)
+                .expireAfterWrite(Duration.ofHours(6))
+                .recordStats();
+    }
+}
